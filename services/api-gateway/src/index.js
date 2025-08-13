1b55bf401571c3b1b4c211af971a469744d67c7d
olamiwale <olamiwalepaul@gmail.com>
2025-08-13 23:16:25 +0100

    file tress setuo

---
+++ services/api-gateway/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', service: 'api-service' });
+});
+
+app.listen(3000, () => {
+  console.log('Users service running on port 3000');
+});
